0b5fa548ee8a01006b5d97580a9ae52a8dda1b01
mathi-vm <160345291+mathi-vm@users.noreply.github.com>
2024-07-20 02:01:55 +0530

    Add files via upload

---
+++ backend/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const ConnectDB = require("./config/db");
+require("dotenv").config();
+const userRoute = require("./routes/userRoutes");
+const journalRoute = require("./routes/JournalRoutes");
+
+const cors = require("cors");
+const app = express();
+
+app.use(express.json());
+
+ConnectDB();
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+
+    allowedHeaders: "Content-Type,Authorization", 
+  })
+);
+
+//check api
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+app.use(`/api/users`, userRoute);
+app.use("/api/journal", journalRoute);
+
+
+app.listen(process.env.PORT, () =>
+  console.log(`server started on port: ${process.env.PORT}`)
+);
